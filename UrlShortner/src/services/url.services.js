99e6b19586742976bbf6d36c15a5259565eb62d1
ankitkandwal-git <sharmaankit49103@gmail.com>
2026-04-11 14:43:00 +0530

    Backend

--- UrlShortner/src/services/url.services.js
+++ UrlShortner/src/services/url.services.js
@@ -1,10 +1,10 @@
 const { nanoid } = require('nanoid');
-const { createUser } = require('../models/user.model');
+const { createUrl } = require('../models/user.model');
 const createShortUrlService = async (db, longUrl) => {
     const shortCode = nanoid(7);
     const baseUrl = 'http://localhost:3000';
     const shortUrl = `${baseUrl}/${shortCode}`;
-    await createUser(db, { shortCode, longUrl, shortUrl });
+    await createUrl(db, { shortCode, longUrl, shortUrl });
     return {
         shortCode,
         longUrl,
